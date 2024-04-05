3348d9d8416f90e139fe6d6865f46e6804af7a0e
“Zahed75” <zahedhasan.cs@gmail.com>
2024-04-06 03:38:25 +0600

    modified:   src/modules/Admin/model.js

--- src/modules/Admin/model.js
+++ src/modules/Admin/model.js
@@ -5,15 +5,28 @@ const mongoose = require('mongoose');
 
 
 const adminSchema = new mongoose.Schema({
-
+    UserId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'user'
+    },
 
     carousel: {
         type : String,
     },
     carouselLink:{
         type : String,
-    }
+    },
 
+    goldBeans:{
+        type:Number,
+        default:0
+    },
+    diamond:{
+        type:Number,
+        default:0
+    },
+    
 
 },{versionKey:false})
 
