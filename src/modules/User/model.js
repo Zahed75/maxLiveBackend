7c46de71cdb6a51b41238bdb883f852d33e07a12
“Zahed75” <zahedhasan.cs@gmail.com>
2024-04-06 02:13:04 +0600

    User PhobeNumber Validation Done

--- src/modules/User/model.js
+++ src/modules/User/model.js
@@ -161,9 +161,14 @@ UserSchema.pre('save', async function hashPassword(next) {
       return await bcrypt.compare(password, this.password);
     },
   };
+
   
   //Validations
-
+  UserSchema.path('agencyNumber').validate(function (value) {
+    const regex = /^\d{13}$/; // regular expression to match 11 digits
+    return regex.test(value);
+  }, 'Must be a valid phone number');
+  
 
   
   const UserModel = mongoose.model('user', UserSchema);
