b81cb52655d3acdc7e4ac947c3b7a9a663eb5473
“Zahed75” <zahedhasan.cs@gmail.com>
2024-05-21 14:28:01 +0600

    docker-compose.yml update

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,56 +1,13 @@
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-
-
-// // create a connect
-// const uri = process.env.MONGODB_URI;
-
-// const connectWithDB = async () => {
-//   try {
-//     const db = await mongoose.connect(uri);
-//     console.log('Connected to Database');
-//     return db;
-//   } catch (e) {
-//     console.log(e);
-//     process.exit(1);
-//   }
-// };
-
-// const disconnectFromDB = () => {
-//   return mongoose.connection.close();
-// };
-
-// // export them
-// module.exports = {
-//   connectWithDB,
-//   disconnectFromDB,
-// };
-
-
 require('dotenv').config();
 const mongoose = require('mongoose');
-const fs = require('fs');
 
-// Read the MongoDB URI from the secret file if it exists
-let uri;
-if (process.env.MONGODB_URI_FILE) {
-  try {
-    uri = fs.readFileSync(process.env.MONGODB_URI_FILE, 'utf8').trim();
-  } catch (error) {
-    console.error('Error reading MongoDB URI from file:', error);
-    process.exit(1);
-  }
-} else {
-  uri = process.env.MONGODB_URI;
-}
 
-// Create a connect function
+// create a connect
+const uri = process.env.MONGODB_URI;
+
 const connectWithDB = async () => {
   try {
-    const db = await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(uri);
     console.log('Connected to Database');
     return db;
   } catch (e) {
@@ -63,8 +20,10 @@ const disconnectFromDB = () => {
   return mongoose.connection.close();
 };
 
-// Export them
+// export them
 module.exports = {
   connectWithDB,
   disconnectFromDB,
 };
+
+
