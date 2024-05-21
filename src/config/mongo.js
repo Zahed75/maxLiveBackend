81920f97913d17639f24a1366a517aafdd3b5d78
“Zahed75” <zahedhasan.cs@gmail.com>
2024-05-21 12:56:24 +0600

    docker-compose.yml update

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,12 +1,56 @@
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+
+
+// // create a connect
+// const uri = process.env.MONGODB_URI;
+
+// const connectWithDB = async () => {
+//   try {
+//     const db = await mongoose.connect(uri);
+//     console.log('Connected to Database');
+//     return db;
+//   } catch (e) {
+//     console.log(e);
+//     process.exit(1);
+//   }
+// };
+
+// const disconnectFromDB = () => {
+//   return mongoose.connection.close();
+// };
+
+// // export them
+// module.exports = {
+//   connectWithDB,
+//   disconnectFromDB,
+// };
+
+
 require('dotenv').config();
 const mongoose = require('mongoose');
+const fs = require('fs');
 
-// create a connect
-const uri = process.env.MONGODB_URI;
+// Read the MongoDB URI from the secret file if it exists
+let uri;
+if (process.env.MONGODB_URI_FILE) {
+  try {
+    uri = fs.readFileSync(process.env.MONGODB_URI_FILE, 'utf8').trim();
+  } catch (error) {
+    console.error('Error reading MongoDB URI from file:', error);
+    process.exit(1);
+  }
+} else {
+  uri = process.env.MONGODB_URI;
+}
 
+// Create a connect function
 const connectWithDB = async () => {
   try {
-    const db = await mongoose.connect(uri);
+    const db = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Connected to Database');
     return db;
   } catch (e) {
@@ -19,7 +63,7 @@ const disconnectFromDB = () => {
   return mongoose.connection.close();
 };
 
-// export them
+// Export them
 module.exports = {
   connectWithDB,
   disconnectFromDB,
--- src/modules/User/model.js
+++ src/modules/User/model.js
@@ -123,6 +123,13 @@ const UserSchema = new mongoose.Schema(
     },
     role: {
       type: String,
+      // BU -> BASIC USER
+      // HO -> HOST
+      //AG-AGENCY 
+      //MP -> MASTER PORTAL
+      // AD -> ADMIN
+      //CN - COIN 
+      // BR -BEAN RESELLER
       enum: ["BU", "HO", "AG", "MP", "AD", "CN", "BR"],
       required: true,
     },
