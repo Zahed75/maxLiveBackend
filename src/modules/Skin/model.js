a690448347d5062364e14380f5885a5e50ce4876
Nasirul-Islam-Chowdhury <nasirchy252@gmail.com>
2024-06-26 15:53:45 +0600

    skin issue fixed

--- src/modules/Skin/model.js
+++ src/modules/Skin/model.js
@@ -1,36 +1,37 @@
 const { NameModule } = require("@faker-js/faker");
 const mongoose = require("mongoose");
 
-const skinSchema = new mongoose.Schema({
-  file: {
-    type: String,
-    required: false,
-    default: ""
+const skinSchema = new mongoose.Schema(
+  {
+    file: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["ENTRY", "RIDE", "FRAME"],
+    },
+    fileType: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    beans: [
+      {
+        time: String,
+        value: Number,
+      },
+    ],
   },
-  type: {
-    type: String,
-    required: true,
-    enum: ["ENTRY", "RIDE", "FRAME"],
-  },
-  fileType: {
-    type: String,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  beans: {
-    type: [{
-      time: String,
-      value: Number
-    }],
-    required: true,
-  },
-},
-{
-  versionKey: false
-});
-  
+  {
+    versionKey: false,
+  }
+);
+
 const Skin = mongoose.model("Skin", skinSchema);
 
 module.exports = { Skin };
