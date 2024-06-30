f7e3fa576d33a93e705fa14e04ef041a5658ee40
Safayat <safayatofficial1@gmail.com>
2024-06-30 11:21:21 +0600

    duration added on seat beside host

--- src/modules/SeatBesideHost/service.js
+++ src/modules/SeatBesideHost/service.js
@@ -1,6 +1,17 @@
+const dayjs = require("dayjs");
 const { SeatBesideHost } = require("./model");
 
 const createSeatBesideHostService = async (payload) => {
+  const days = parseInt(payload.time, 10);
+  const totalSeconds = days * 24 * 60 * 60; // Convert days to seconds
+
+  const timeDuration = dayjs.duration(totalSeconds, "seconds");
+  const hours = Math.floor(totalSeconds / 3600).toString();
+  const minutes = timeDuration.minutes().toString().padStart(2, '0');
+  const seconds = timeDuration.seconds().toString().padStart(2, '0');
+  const milliseconds = timeDuration.milliseconds().toString().padStart(6, '0');
+
+  payload.time = `${hours}:${minutes}:${seconds}.${milliseconds}`;
   const isSkinExistsWithSameTimeAndBeans = await SeatBesideHost.findOne({
     time: payload.time,
   });
@@ -25,7 +36,7 @@ const getAllSeatBesideHostService = async () => {
 };
 
 const updateSeatBesideHostService = async (payload, id) => {
-  const result = await SeatBesideHost.findByIdAndUpdate(id, {beans: payload.beans});
+  const result = await SeatBesideHost.findByIdAndUpdate(id, { beans: payload.beans });
   return result;
 };
 
